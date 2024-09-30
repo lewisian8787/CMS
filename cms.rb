4c3a93454344dff0a9ce80ea77741022b2b0a4b2
Ian Lewis <lewisian8787@gmail.com>
2024-09-30 20:12:19 +0000

    finishing up cms.rb file

--- cms.rb
+++ cms.rb
@@ -1,6 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
 require "redcarpet"
+#require 'bcrypt'
+require "yaml"
 
 #configuring sessions
 configure do
@@ -8,33 +10,67 @@ configure do
   set :session_secret, SecureRandom.hex(32)
 end
 
-#render markdown method
-def render_markdown(markdown_string)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    markdown.render(markdown_string)
-end
+helpers do
+  
+  def load_user_credentials
+    if ENV["RACK_ENV"] == "test"
+      credentials_path = File.expand_path("../test/users.yml", __FILE__)
+      # "/home/ec2-user/environment/CMS/test/data"
+    else
+      credentials_path = File.expand_path("../users.yml", __FILE__)
+      # "/home/ec2-user/environment/CMS/data"
+    end
+    YAML.load_file(credentials_path)
+  end
+  
+  def signin_status
+    session[:signin_status]
+  end
 
+  def valid_credentials?
+    if load_user_credentials.key?(params[:username])
+      bcrypt_password = BCrypt::Password.new(load_user_credentials[params[:username]])
+      bcrypt_password == params[:password]
+    else
+      false
+    end
+  end
+  
+  def restricted_message
+    if !signin_status == true
+      session[:message] = "You are not allowed to do that"
+      redirect "/"
+    end
+  end
+  
+  #render markdown method
+  def render_markdown(markdown_string)
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+      markdown.render(markdown_string)
+  end
+  
+  #method to load the content of the file at path requested
+  def load_file_content(path)
+    content = File.read(path)
+    case File.extname(path)
+    when ".txt"
+      headers["Content-Type"] = "text/plain"
+      content
+    when ".md"
+      erb render_markdown(content)
+    end
+  end
+end
+  
 # new method to establish the correct path based on the current environment (determine if its in test or not)
 def data_path
-  if ENV["RACK_ENV"] == "test"
-    File.expand_path("../test/data", __FILE__)
-    # "/home/ec2-user/environment/CMS/test/data"
-  else
-    File.expand_path("../data", __FILE__)
-    # "/home/ec2-user/environment/CMS/data"
-  end
-end
-
-#method to load the content of the file at path requested
-def load_file_content(path)
-  content = File.read(path)
-  case File.extname(path)
-  when ".txt"
-    headers["Content-Type"] = "text/plain"
-    content
-  when ".md"
-    erb render_markdown(content)
-  end
+    if ENV["RACK_ENV"] == "test"
+      File.expand_path("../test/data", __FILE__)
+      # "/home/ec2-user/environment/CMS/test/data"
+    else
+      File.expand_path("../data", __FILE__)
+      # "/home/ec2-user/environment/CMS/data"
+    end
 end
 
 # Displays the index of files
@@ -51,8 +87,33 @@ get "/" do
   erb :index
 end
 
-get "/new" do 
+get "/new_document" do
+  restricted_message
   erb :new, layout: :layout
+end
+
+get "/signin" do
+  erb :signin, layout: :layout
+end
+
+post "/signin_evaluation" do
+  puts params.inspect
+  if valid_credentials?
+    session[:message] = "Welcome #{params[:username]}"
+    session[:username] = params[:username]
+    session[:signin_status] = true
+    redirect "/"
+  else
+    session[:message] = "Wrong bitch"
+    session[:signin_status] = false
+    redirect "/signin"
+  end
+end
+
+post "/signout" do
+  session[:signin_status] = false
+  session[:message] = "#{session[:username]} has been signed out."
+  redirect "/"
 end
 
 get "/:filename" do
@@ -69,6 +130,7 @@ get "/:filename" do
 end
 
 get "/:filename/edit" do
+  restricted_message
   file_path = File.join(data_path, params[:filename])
   
   @filename = params[:filename]
@@ -77,53 +139,42 @@ get "/:filename/edit" do
   erb :edit
 end
 
-# post "/create" do
-  
-#   filename = params[:filename].to_s
-  
-#   if filename.size == 0
-#     session[:message] = "You must enter a name"
-#     status 402
-#     redirect "/new"
-#   else
-#     file_path = File.join(data_path, filename)
-    
-#     File.write(file_path, "")
-    
-#     session[:message] = "#{params[:filename]} has been created"
-#     redirect "/"
-#   end
-# end
-
-# cms.rb
-get "/new" do
-  erb :new
-end
-
-post "/create" do
+post "/create_new_page" do
+  restricted_message
   filename = params[:filename].to_s
-
+  
   if filename.size == 0
-    session[:message] = "A name is required."
-    status 422
-    erb :new
+    session[:message] = "You must enter a name"
+    status 402
+    redirect "/new"
   else
     file_path = File.join(data_path, filename)
-
+    
     File.write(file_path, "")
-    session[:message] = "#{params[:filename]} has been created."
-
+    
+    session[:message] = "#{params[:filename]} has been created"
     redirect "/"
   end
 end
 
-post "/:filename" do
+post "/:filename/update" do
+  restricted_message
   file_path = File.join(data_path, params[:filename])
   
   File.write(file_path, params[:content])
   
 
   session[:message] = "#{params[:filename]} has been updated"
+  redirect "/"
+end
+
+post "/:filename/delete_file" do
+  restricted_message
+  file_path = File.join(data_path, params[:filename])
+  
+  File.delete(file_path)
+  
+  session[:message] = "#{params[:filename]} has been deleted"
   redirect "/"
 end
 
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -1,4 +1,3 @@
-# test/cms_test.rb
 ENV["RACK_ENV"] = "test"
 
 require "fileutils"
@@ -27,6 +26,10 @@ class CMSTest < Minitest::Test
     File.open(File.join(data_path, name), "w") do |file|
       file.write(content)
     end
+  end
+
+  def session
+    last_request.env["rack.session"]
   end
 
   def test_index
@@ -65,11 +68,7 @@ class CMSTest < Minitest::Test
     get "/notafile.ext"
 
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "notafile.ext does not exist"
+    assert_equal "notafile.ext does not exist.", session[:message]
   end
 
   def test_editing_document
@@ -86,13 +85,81 @@ class CMSTest < Minitest::Test
     post "/changes.txt", content: "new content"
 
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-
-    assert_includes last_response.body, "changes.txt has been updated"
+    assert_equal "changes.txt has been updated.", session[:message]
 
     get "/changes.txt"
     assert_equal 200, last_response.status
     assert_includes last_response.body, "new content"
+  end
+
+  def test_view_new_document_form
+    get "/new"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_create_new_document
+    post "/create", filename: "test.txt"
+    assert_equal 302, last_response.status
+    assert_equal "test.txt has been created.", session[:message]
+
+    get "/"
+    assert_includes last_response.body, "test.txt"
+  end
+
+  def test_create_new_document_without_filename
+    post "/create", filename: ""
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, "A name is required"
+  end
+
+  def test_deleting_document
+    create_document("test.txt")
+
+    post "/test.txt/delete"
+    assert_equal 302, last_response.status
+    assert_equal "test.txt has been deleted.", session[:message]
+
+    get "/"
+    refute_includes last_response.body, %q(href="/test.txt")
+  end
+
+  def test_signin_form
+    get "/users/signin"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_signin
+    post "/users/signin", username: "admin", password: "secret"
+    assert_equal 302, last_response.status
+    assert_equal "Welcome!", session[:message]
+    assert_equal "admin", session[:username]
+
+    get last_response["Location"]
+    assert_includes last_response.body, "Signed in as admin"
+  end
+
+  def test_signin_with_bad_credentials
+    post "/users/signin", username: "guest", password: "shhhh"
+    assert_equal 422, last_response.status
+    assert_nil session[:username]
+    assert_includes last_response.body, "Invalid credentials"
+  end
+
+  def test_signout
+    get "/", {}, {"rack.session" => { username: "admin" } }
+    assert_includes last_response.body, "Signed in as admin"
+
+    post "/users/signout"
+    assert_equal "You have been signed out.", session[:message]
+
+    get last_response["Location"]
+    assert_nil session[:username]
+    assert_includes last_response.body, "Sign In"
   end
 end
